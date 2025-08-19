682217825fe9a92b9976f05d02ddc52f9029ce12
yeungbriannaj <briannajyeung@gmail.com>
2025-08-20 11:22:05 +1200

    Wsgi.py and __init__

--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/recipes")
-def recipes():
-    return render_template("recipes.html")
-
 if __name__ == "__main__":
     app.run(debug=True)
